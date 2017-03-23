e54c65d8f77cefb41c9a9febe11de36af3ac3ad9
Steve Bulgin <steven.bulgin@gmail.com>
2017-03-23 12:55:16 -0400

    Added remaining activities

---
+++ BeeApiaryCapstone-master/app/src/main/java/com/example/tanisha/busy_bees_capstone_project/InspectionActivity.java
@@ -0,0 +1,13 @@
+package com.example.tanisha.busy_bees_capstone_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InspectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inspection);
+    }
+}
---
+++ BeeApiaryCapstone-master/app/src/main/java/com/example/tanisha/busy_bees_capstone_project/QueenActivity.java
@@ -0,0 +1,13 @@
+package com.example.tanisha.busy_bees_capstone_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QueenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_queen);
+    }
+}
